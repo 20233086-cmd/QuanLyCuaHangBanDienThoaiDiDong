3bfa058bac948bc02e76dcbb788904c397a218f9
NguyenHuuHa <20233086@eaut.edu.vn>
2025-10-20 08:57:53 +0700

    Update ITaiKhoanService.cs

--- QuanLyCuaHangDienThoai/Interface/ITaiKhoanService.cs
+++ QuanLyCuaHangDienThoai/Interface/ITaiKhoanService.cs
@@ -1,1 +1,13 @@
+using System.Data;
+using QuanLyCuaHangDienThoai.DTO;
 
+namespace QuanLyCuaHangDienThoai.Interface
+{
+    public interface ITaiKhoanService
+    {
+        bool KiemTraDangNhap(string tenDangNhap, string matKhau);
+        bool DangKyTaiKhoan(TaiKhoanDTO tk);
+        TaiKhoanDTO LayTaiKhoanTheoTen(string tenDangNhap);
+        DataTable LayTatCaTaiKhoan();
+    }
+}
