004b1c11c40943e4538ebc6b11d567b1b6d82acf
NguyenHuuHa <20233086@eaut.edu.vn>
2025-10-20 08:54:54 +0700

    Add files via upload

---
+++ QuanLyCuaHangDienThoai/BUS/TaiKhoanBUS.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using QuanLyCuaHangDienThoai.DTO;
+using QuanLyCuaHangDienThoai.DAL;
+
+namespace QuanLyCuaHangDienThoai.BUS
+{
+    public class TaiKhoanBUS
+    {
+        private TaiKhoanDAL dal = new TaiKhoanDAL();
+
+        public bool DangNhap(string ten, string mk)
+        {
+            return dal.KiemTraDangNhap(ten, mk);
+        }
+
+        public bool DangKy(TaiKhoanDTO tk)
+        {
+            return dal.DangKyTaiKhoan(tk);
+        }
+
+        public TaiKhoanDTO LayTheoTen(string ten)
+        {
+            return dal.LayTaiKhoanTheoTen(ten);
+        }
+
+        public DataTable LayTatCa()
+        {
+            return dal.LayTatCaTaiKhoan();
+        }
+    }
+}
