bf3acacb05042c6fc0183eebc5f8fe29b80a5d32
NguyenHuuHa <20233086@eaut.edu.vn>
2025-10-20 09:27:21 +0700

    Add files via upload

---
+++ QuanLyCuaHangDienThoai/DAL/DatabaseHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace QuanLyCuaHangDienThoai.DAL
+{
+    public class DatabaseHelper
+    {
+        private static string chuoiKetNoi =
+            @"Data Source=HAZY\SQLEXPRESS;Initial Catalog=QuanLyCuaHangDT;Integrated Security=True;TrustServerCertificate=True";
+
+        public static SqlConnection MoKetNoi()
+        {
+            SqlConnection conn = new SqlConnection(chuoiKetNoi);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            return conn;
+        }
+
+        public static void DongKetNoi(SqlConnection conn)
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+    }
+}
