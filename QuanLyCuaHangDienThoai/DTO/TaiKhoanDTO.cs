fbbe86cacc37be601d78c78692a8434b29e8d3f6
NguyenHuuHa <20233086@eaut.edu.vn>
2025-10-20 08:51:27 +0700

    Add files via upload

---
+++ QuanLyCuaHangDienThoai/DTO/TaiKhoanDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuanLyCuaHangDienThoai.DTO
+{
+    public class TaiKhoanDTO
+    {
+        public int MaTK { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhauHash { get; set; }
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string DienThoai { get; set; }
+        public int MaQuyen { get; set; }
+        public bool Is2FAEnabled { get; set; }
+        public DateTime NgayTao { get; set; }
+        public bool TrangThai { get; set; }
+    }
+}
