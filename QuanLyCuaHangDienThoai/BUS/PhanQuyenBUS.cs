db9f9d2f7f67da760fccb9c7cb6c683f8d492bfc
NguyenHuuHa <20233086@eaut.edu.vn>
2025-10-20 08:54:24 +0700

    Update PhanQuyenBUS.cs

--- QuanLyCuaHangDienThoai/BUS/PhanQuyenBUS.cs
+++ QuanLyCuaHangDienThoai/BUS/PhanQuyenBUS.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyCuaHangDienThoai.DAL;
 
+namespace QuanLyCuaHangDienThoai.BUS
+{
+    public class PhanQuyenBUS
+    {
+        private PhanQuyenDAL dal = new PhanQuyenDAL();
+
+        public string LayTenQuyen(int maQuyen)
+        {
+            return dal.LayTenQuyenTheoMa(maQuyen);
+        }
+    }
+}
